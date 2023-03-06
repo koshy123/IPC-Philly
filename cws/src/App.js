c87c98a530a49c08ad0cdeb5d534a033d86e779a
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-03-06 10:18:49 -0500

    add in css styling

--- cws/src/App.js
+++ cws/src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <div className='gradient_bg'>
+      <div className='gradient__bg'>
         <Navbar />
         <Header />
     </div>
