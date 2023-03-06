196e6847e2b651e0dc623929fe6ee38acab27a33
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-03-05 23:29:50 -0500

    add new color for bg

--- cws/src/App.js
+++ cws/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers'
 import { Navbar, Brand, CTA } from './components';
-
+import './App.css'
 function App() {
   return (
     <div className="App">
@@ -16,9 +16,8 @@ function App() {
     <CTA />
     <Blog />
     <Footer />
-    
     </div>
   );
-}
+};
 
 export default App;
