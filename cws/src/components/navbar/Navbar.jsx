9665b8a00ccbb70c15b766a6a6e30dde95b612c1
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-03-07 09:25:02 -0500

    wokring on the navbar

--- cws/src/components/navbar/Navbar.jsx
+++ cws/src/components/navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./navbar.css";
-
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import title from "../../assets/ipcTitle.png"
+//BEM, aka Block Element Modifier
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links_logo'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={title} alt="logo" />
+        </div>
+        <div className='gpt3__navbar-links_container'> 
+          <p></p>
+        </div>
+      </div>
+    </div>
   )
 }
 
