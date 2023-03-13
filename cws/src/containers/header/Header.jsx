70de4b8498c17de07e84e3fdc29606ee24698878
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-03-13 15:34:24 -0400

    add the features of the header component

--- cws/src/containers/header/Header.jsx
+++ cws/src/containers/header/Header.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import "./header.css";
+import "../../App.css"
+import preacher from '../../assets/pastor_preach.jpg'
+import banner from '../../assets/ipcTitle.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='ipc__header section__padding id="home'>
+      <div className='ipc__header-content'>
+        <h1 className='gradient__text'> Lets grow together and become a family</h1>
+
+        <div className='ipc__header-content__input'>
+          <input type="email" placeholder='your email address'></input>
+          <button type='button'>Get Involved</button>
+        </div>
+
+        <div className='ipc__header-content__people'>
+          <img src={banner}  />
+          <p>Taste and see that the Lord is Good</p>
+        </div>
+      </div>
+
+      <div className='ipc__header-image'>
+        <img src={preacher} />
+        <p>And the Word became Flesh</p>
+      </div>
+    </div>
   )
 }
 
-export default Header
+export default Header;
