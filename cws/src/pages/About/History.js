fdb1239670897cad7ddd656b44cf7f10adde6b42
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-04-18 23:43:56 -0400

    create layout for thee about links

---
+++ cws/src/pages/About/History.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './about.css'
+
+
+
+
+const History = () => {
+    return (
+        <div className='about_main'>
+            <div className='about_header'>
+            <div className='about_header_container '>
+                <h1 className='about_header_title'>About Us</h1>
+            </div>
+        </div>
+
+
+
+        </div>
+      )
+    }
+export default History;
