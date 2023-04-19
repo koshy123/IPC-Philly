c0dfb2e07aae5b7999f270a44334f72fcc318533
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-04-18 23:00:17 -0400

    add padding to renew page

--- cws/src/pages/Renew/Renew.js
+++ cws/src/pages/Renew/Renew.js
@@ -6,18 +6,18 @@ import floor from '../../assets/floorSitting.png'
 const Renew = () => {
     return (
         <div className='renew'>
-            <div>
+            <div className='renew_padding'>
             <h1 className='title_for_renew'>Renew </h1>
             <p className='text_for_renew'>Rufus James was inspired to start this this meeting so that young adults can grow deeper in Gods word. From its start it has consistently grown and allowed many to have a greater passion for God.</p>
-
             </div>
         <div className='images_at_renew'>
           <img src={rufus}></img>
           <img src={floor}></img>
-
-
         </div>
-        
+        <div className='renew_padding'>
+            <h1 className='title_for_renew'>Join </h1>
+            <p className='text_for_renew'>Meeting times are.</p>
+            </div>
         </div>
       )
     }
