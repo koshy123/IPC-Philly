77defeef4a53c61eb5aeefbc9774bc7da7f2f4fb
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-04-19 12:49:15 -0400

    finish out the basic styling on the about page

--- cws/src/pages/About/About.js
+++ cws/src/pages/About/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './about.css'
 import church from '../assets/FrontChurch.jpg'
 import kuttu from '../assets/KuttuZephan.jpeg'
-import wesley from '../assets/WesleyHeadshot.png'
+import wesley from '../assets/WesleyPrayStrong.png'
 import { Link } from "react-router-dom";
 
 
@@ -17,12 +17,22 @@ const About = () => {
             </div>
         </div>
 
-
         <div className='meet_the_team'>
-        <p className='items_about'>Our History <Link to='about/history'><img src={church}/></Link></p>
-        <p className='items_about'>Our Pastor <Link to='about/pastor'><img src={wesley} /> </Link></p>
-        <p className='items_about'>Our Team <Link to='about/team'><img src={kuttu} /></Link></p>
+        <div className='meet_the_team_about_box'>
+            <h4 className='items_about'>Our History</h4> <Link to='about/history'>
+            <img src={church}/></Link><p className='about_under_image'>Was founded back in 1990 by the grace of God. To hold a stand of holiness and follow Gods word.</p>
         </div>
+
+        <div className='meet_the_team_about_box'>
+            <h4 className='items_about'>Our Pastor</h4> <Link to='about/pastor'>
+            <img src={wesley} /></Link><p className='about_under_image'>Graduating Bible school, God put on Pastor Wesley Daniel heart to start a church in Philadelphia.</p> 
+        </div>
+        <div className='meet_the_team_about_box'> 
+            <h4 className='items_about'>Our Team </h4>  <Link to='about/team'>
+            <img src={kuttu} /></Link><p className='about_under_image'>Our church would not be able to function without our hardworking team of motivated, God fearing individuals.</p>
+        </div> 
+        </div>
+
         </div>
       )
     }
