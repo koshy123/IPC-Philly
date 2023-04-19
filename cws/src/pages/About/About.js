a812f7eb88b2c593253ab26a17d981dd7d3855cc
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-04-18 23:26:03 -0400

    fill out the about page and add animation to images

--- cws/src/pages/About/About.js
+++ cws/src/pages/About/About.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './about.css'
-import banner from '../assets/AboutBanner.png'
+import church from '../assets/FrontChurch.jpg'
+import kuttu from '../assets/KuttuZephan.jpeg'
+import wesley from '../assets/WesleyHeadshot.png'
+
+
+
 const About = () => {
     return (
         <div className='about_main'>
@@ -9,16 +14,14 @@ const About = () => {
                 <h1 className='about_header_title'>About Us</h1>
             </div>
         </div>
-        <div className='meet_the_team'>
-            <p>Meet Our Team</p>
-          
 
-        </div>
-
-        </div>
         
-       
+        <div className='meet_the_team'>
+        <p className='items_about'>Our History <img src={church}/></p>
+        <p className='items_about'>Our Pastor <img src={wesley} /> </p>
+        <p className='items_about'>Our Team <img src={kuttu} /></p>
+        </div>
+        </div>
       )
     }
-
 export default About;
