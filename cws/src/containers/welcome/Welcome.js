d1f4e9731789849e431ce2a42628332cc4eae950
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-04-22 13:13:55 -0400

    able to add zoom out feature to images

--- cws/src/containers/welcome/Welcome.js
+++ cws/src/containers/welcome/Welcome.js
@@ -5,12 +5,11 @@ const Welcome = () => {
   return (
     <div className="welcome_flex">
         <div className="welcome_left_text">
-            <h1>Welcome</h1>
-            <p>jsadifjnasfgkjsnfjsnfosjdnfoskdjcnoskcnsdkvjnlo</p>
+            <h1>Welcome!</h1>
+            <p>Welcome to IPC Philly. We are so glad that you are taking interest in our church. Feel free to visit us during Sunday Service. We are always ready to pray and worship with you. God bless.</p>
         </div>
 
         <div className="welcome_right_image">
-            <img src= {joshua} />
         </div>
     </div>
   );
