1396a3db35997551d2cd56f7cedf4728ec05b0c5
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-04-23 07:31:58 -0400

    add

--- cws/src/containers/header/ContactForm.js
+++ cws/src/containers/header/ContactForm.js
@@ -4,7 +4,7 @@ import "./header.scss";
 
 const ContactForm = () => {
     return (
-      <div className='contact_form'>
+      <div className=''>
         <form>
           <label htmlFor="name">Name:</label>
           <input type="text" id="name" name="name" placeholder="Enter your name" />
@@ -18,7 +18,7 @@ const ContactForm = () => {
           <label htmlFor="message">Message:</label>
           <textarea id="message" name="message" placeholder="Enter your message"></textarea>
   
-          <button type="submit">Submit</button>
+          <button className='contact_form_button' type="submit">Submit</button>
         </form>
       </div>
     );
