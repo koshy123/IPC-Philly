ba1bfad869aa64d74f3c80ba3384e4408b7b7dac
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-04-28 16:50:16 -0400

    able to het the html linked up with the backend and it works. now need to clean up

--- cws/src/containers/header/ContactForm.js
+++ cws/src/containers/header/ContactForm.js
@@ -1,24 +1,53 @@
 import React from 'react';
 import "./header.scss";
+import axios from "axios";
+import { useState } from "react";
 
 const ContactForm = () => {
 
+  const [email, setEmail] = useState("");
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      email,
+    };
+
+    const response = await axios.post(
+      "http://localhost:5000/api/sendemail",
+      data
+    );
+    console.log(response.data);
+  };
+
     return (
       <div className='contact_form'>
-        <form>
+        <form className='"--form-control' onSubmit={sendEmail}>
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" placeholder="Enter your name" />
+          <input 
+          type="text" 
+          id="name" 
+          name="name" 
+          placeholder="Enter your name" />
   
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" placeholder="Enter your email address" />
-  
-          <label htmlFor="subject">Subject:</label>
-          <input type="text" id="subject" name="subject" placeholder="Enter the subject of your message" />
+          <input 
+          type="email" 
+          id="email" 
+          name="email" 
+          placeholder="Enter your email address"            
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} />
   
           <label htmlFor="message">Message:</label>
           <textarea id="message" name="message" placeholder="Enter your message"></textarea>
   
-          <button className='contact_form_button' type="submit">Submit</button>
+          <button 
+          className='contact_form_button' 
+          type="submit">
+          Submit
+          </button>
         </form>
       </div>
     );
