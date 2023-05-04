c4af5ca65f7b6842b268fe95f652c84b040960b8
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-05-04 13:43:21 -0400

    sunday school

--- cws/src/pages/Involved/SundaySchool/SundaySchool.js
+++ cws/src/pages/Involved/SundaySchool/SundaySchool.js
@@ -7,8 +7,8 @@ const SundaySchool = () => {
     return (
         <div className='renew'>
             <div className='renew_padding'>
-            <h1 className='title_for_renew'>Renew </h1>
-            <p className='text_for_renew'>Rufus James was inspired to start this this meeting so that young adults can grow deeper in Gods word. From its start it has consistently grown and allowed many to have a greater passion for God.</p>
+            <h1 className='title_for_renew'>Sunday School </h1>
+            <p className='text_for_renew'>Our award winning Sunday School staff and students study the word of God with passion.</p>
             </div>
         <div className='images_at_renew'>
           <img src={rufus}></img>
