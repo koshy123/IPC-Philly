82f914ac039d44fbcc7f359963c19ef28b9d11a8
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-05-08 10:22:55 -0400

    organize files better

--- cws/src/pages/About/Pastor.js
+++ cws/src/pages/About/Pastor.js
@@ -6,10 +6,10 @@ const Pastor = () => {
     return (
         <div className='pastor_main'>
             <div className='pastor_header'>
-            <div className='pastor_header_container '>
+
                 <h1 className='pastor_header_title'>Pastor </h1>
+
             </div>
-        </div>
         <div>
             <h1>Pastor Wesley Daniel</h1>
             <p></p>
