d45893519d111e89e4044c209e9627bad9984cb5
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-05-17 09:30:34 -0400

    save info on History

--- cws/src/pages/About/Pastor/Pastor.js
+++ cws/src/pages/About/Pastor/Pastor.js
@@ -11,7 +11,7 @@ const Pastor = () => {
             </div>
         <div>
             <h1>Pastor Wesley Daniel</h1>
-            <p></p>
+            <p>Growing up Wesley Pastor never imagined he would be a pastor, but God called him into the ministry. After graduating from Faith Seminary, led by the late Dr. T.G Koshy from 1982-1986. Later moving to Philadelphia after his marriage and then starting his church. Welsey Daniel has two wonderful children who are married and 2 amazing grandchildren. </p>
         </div>
 
 
