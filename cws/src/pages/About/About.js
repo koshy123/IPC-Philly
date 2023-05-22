9f402d8f9d34da748df0457a46b2b817aa1b399c
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-05-22 00:29:02 -0400

    clean about page

--- cws/src/pages/About/About.js
+++ cws/src/pages/About/About.js
@@ -17,14 +17,12 @@ const About = () => {
         <div className='meet_the_team'>
         <div className='meet_the_team_about_box'>
             <Link to='/history'>
-            <h4 className='items_about'> History</h4> 
             <img src={church}></img></Link>
             {/* <p className='about_under_image'>Was founded back in 1990 by the grace of God. To hold a stand of holiness and follow Gods word.</p> */}
         </div>
 
         <div className='meet_the_team_about_box'>
             <Link to='/pastor'>
-            <h4 className='items_about'> Pastor</h4> 
             <img src={wesley} /></Link>
             {/* <p className='about_under_image'>Graduating Bible school, God put on Pastor Wesley Daniel heart to start a church in Philadelphia.</p>  */}
         </div>
