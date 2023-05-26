a9de491f430d62f2e8e85dafb852a64bd4e5d93d
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-05-25 21:57:08 -0400

    formated for mobile as well

--- cws/src/pages/Involved/Renew/Renew.js
+++ cws/src/pages/Involved/Renew/Renew.js
@@ -24,7 +24,7 @@ const Renew = () => {
             <p className='bottom_text_about'>IPC Philly Renew is a youth ministry designed to engage young people with teaching and discussion of the Word. We have been fortunate enough to see young people from all over the city of Philadelphia gather and spend time in worship, fellowship, and meditation of the scriptures. Our topics are either teaching series with speakers from the city or a book studies. Our prayer is to equip this generation with God’s Word and send them out into schools, campuses, and workplaces as heralds of the Gospel. </p>
             <div>
               <p className='schedule_about'>
-                <b>When:</b> Meetings are twice a month.(please dm us for more accurate dates and times) <a target='blank' href='https://www.instagram.com/ipcphillyyouth/'>Renew Insta.</a>
+                <b>When:</b> Meetings are twice a month.(message us for dates and times) <a target='blank' href='https://www.instagram.com/ipcphillyyouth/'>Renew Insta.</a>
               </p>
             </div>
         </div>
